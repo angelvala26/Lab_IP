3849356cb0a2f8ab74bbbeeafbdbd7d9c9068c0d
Angel Valadez <angelvaladezt@gmail.com>
2026-08-24 19:46:25 -0600

    UDPATE octal compacto.py

--- octal compacto.py
+++ octal compacto.py
@@ -1,4 +1,4 @@
-numero, octal = 8,""
-if numero == 0: print("0")
-while numero > 0: octal, numero = str( numero % 8)+ octal, numero // 8
-print(octal)
+numero, octal = 8,"" #Se inicializa la variable con un string vacio
+if numero == 0: print("0") #Si el numero es 0, imprime directamente 0 
+while numero > 0: octal, numero = str( numero % 8)+ octal, numero // 8 # Mientras sea mayor a 0, anade el residuo a OCTAL y divide el numero entre 8 
+print(octal) #Imprime la cadena con el resultado final
