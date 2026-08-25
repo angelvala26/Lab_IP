8a04d35c3956eb3ee48f7b5c040d1ab4de3a22a3
Angel Valadez <angelvaladezt@gmail.com>
2026-08-24 19:53:42 -0600

    UDPATE binario compacto.py

--- binario compacto.py
+++ binario compacto.py
@@ -1,4 +1,4 @@
-numero, binario = 8,""
-if numero == 0: print("0")
-while numero > 0: binario, numero = str( numero % 2)+binario, numero // 2
-print(binario)
+numero, binario = 8,"" #Se inicializa la variable con un string vacio
+if numero == 0: print("0") #Si el numero es 0, imprime directamente 0 
+while numero > 0: binario, numero = str( numero % 2)+binario, numero // 2 # Mientras sea mayor a 0, anade el residuo a binario y divide el numero entre 2 
+print(binario) #Imprime la cadena con el resultado final
