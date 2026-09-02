fe37981a4b8fca8a2d06fed161187976c2b9db2b
Angel Valadez <angelvaladezt@gmail.com>
2026-09-02 17:59:54 -0600

    UDPATE Folders Structure

---
+++ UNIDAD2/01_condiciones.py
@@ -0,0 +1,7 @@
+print(bool(1))
+print(bool(0))
+print(bool(-1))
+print(bool(10000))
+ptint(bool(10.5))
+print(bool("A"))
+#Todo valor diferente de 0, su conversion booleana dara como resultado True
---
+++ UNIDAD2/02_operadores.py
@@ -0,0 +1,6 @@
+operador = input("Inserta un operador(+,-,*,/):")
+o1= input (str("Introduce un operador:"))
+o2 = input (str("Introduce un operador:"))
+resultado = o1+operador+o2
+resultado = eval(resultado)
+print(resultado)
