976e8858a785869b9f984c20b06c1472659424c4
Angel Valadez <angelvaladezt@gmail.com>
2026-09-02 18:57:58 -0600

    UDPATE Folders Structure

--- Unidad1/06_hexadecimal_compacto.py
+++ Unidad1/06_hexadecimal_compacto.py
@@ -1,22 +1,7 @@
-numero = 8
-if numero == 0:
-    print("0")
-else:
-    hexadecimal = ""
-    while numero > 0:
-        residuo = numero % 16
-        if residuo == 10:
-            residuo = "a"
-        if residuo == 11:
-            residuo = "B"
-        if residuo == 12:
-            residuo = "C"
-        if residuo == 13:
-            residuo = "d"
-        if residuo == 14:
-            residuo = "E"
-        if residuo == 15:
-            residuo = "f"
-        hexadecimal = str(residuo) + hexadecimal
-        numero = numero // 16
-    print(hexadecimal)
+numero, hexadecimal = 11, "" #Definimos nuestro numero y le damos a hexadecimal una cadena vacia
+if numero==0: print("0") #Si el numero es 0, pues imprime 0
+while numero > 0: #Inicia un ciclo que se repite mientras el numero sea mayor a 0
+    residuo = numero % 16 #Obtiene el residuo al dividir entre el 16
+    hexadecimal = "0123456789ABCDEF" [residuo] + hexadecimal #Usa el residuo para buscar el caracter y lo agrega al resultado
+    numero = numero // 16 #Se divide el numero entre 16
+print(hexadecimal) #Nos muestra el resultadp final de esta conversion
